18fb042e9bb8a3c3ae924288d588eddd52250f87
Rahman1014 <Rahman1014@outlook.com>
2024-10-14 18:11:18 +0400

    Update port number

--- server.js
+++ server.js
@@ -26,6 +26,6 @@ if (process.env.NODE_ENV === 'production') {
   });
 }
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5005;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
